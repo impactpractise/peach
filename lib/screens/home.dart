0f73c71ee5de59ac24dd13790529250c2661d732
Andre <andre.bappert@gmail.com>
2020-02-28 19:55:26 +0100

    hangle google signIn & signOut

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +11,49 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool isAuthorized = false;
 
+  @override
+  void initState() {
+    super.initState();
+    // detects when user signed in
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print('Error signin in: $err');
+    });
+    // Reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    }).catchError((err) {
+      print('Error signin in: $err');
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      print('User signed in!: $account');
+      setState(() {
+        isAuthorized = true;
+      });
+    } else {
+      setState(() {
+        isAuthorized = false;
+      });
+    }
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
   Widget buildAuthScreen() {
-    return Text('Authenticated');
+    return RaisedButton(
+      child: Text('Logout'),
+      onPressed: logout,
+    );
   }
 
   Scaffold buildUnAuthScreen() {
@@ -36,7 +80,7 @@ class _HomeState extends State<Home> {
                     fontSize: 90.0,
                     color: Colors.white)),
             GestureDetector(
-                onTap: () => print('tapped'),
+                onTap: () => login(),
                 child: Container(
                   width: 240,
                   height: 50,
