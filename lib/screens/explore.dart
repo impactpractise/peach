9ac76a40a9e946e62740d00644a9f24b94b24783
Andre <andre.bappert@gmail.com>
2020-03-03 10:07:59 +0100

    add CRUD functionality for users

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -12,6 +12,34 @@ class Explore extends StatefulWidget {
 
 class _ExploreState extends State<Explore> {
   @override
+  void initState() {
+    //createUser();
+    //updateUser();
+    deleteUser();
+    super.initState();
+  }
+
+  createUser() async {
+    await usersRef
+        .document("uniqueSampleString")
+        .setData({"username": "Niklas", "postsCount": 8, "isAdmin": false});
+  }
+
+  updateUser() async {
+    final doc = await usersRef.document("uniqueSampleString").get();
+    if (doc.exists) {
+      doc.reference.updateData({"username": "Niklas Donges"});
+    }
+  }
+
+  deleteUser() async {
+    final DocumentSnapshot doc =
+        await usersRef.document("uniqueSampleString").get();
+    if (doc.exists) {
+      doc.reference.delete();
+    }
+  }
+
   Widget build(context) {
     return Scaffold(
         appBar: header(context, isAppTitle: true),
