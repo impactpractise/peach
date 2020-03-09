0a8f7410778210533de2b0096c2f68b13d815b78
Andre <andre.bappert@gmail.com>
2020-03-09 14:53:08 +0100

    feat/data: query activity data for current user

--- lib/screens/activity_feed.dart
+++ lib/screens/activity_feed.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:peach/screens/home.dart';
 import 'package:peach/widgets/header.dart';
+import 'package:peach/widgets/loading.dart';
 
 class ActivityFeed extends StatefulWidget {
   @override
@@ -7,10 +10,32 @@ class ActivityFeed extends StatefulWidget {
 }
 
 class _ActivityFeedState extends State<ActivityFeed> {
+  getActivityFeed() async {
+    QuerySnapshot snapshot = await activityFeedRef
+        .document(currentUser.id)
+        .collection('feedItems')
+        .orderBy('timestamp', descending: true)
+        .limit(30)
+        .getDocuments();
+    snapshot.documents
+        .forEach((doc) => print('Activity feed item ${doc.data}'));
+    return snapshot.documents;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: 'Activity Feed'),
+      body: Container(
+        child: FutureBuilder(
+            future: getActivityFeed(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return circularProgress(context);
+              }
+              return Text('Acitivty Feed');
+            }),
+      ),
     );
   }
 }
