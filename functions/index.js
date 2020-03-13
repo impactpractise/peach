19aff3088bb27340765dfc601d521b0322e3826c
Andre <andre.bappert@gmail.com>
2020-03-13 13:54:10 +0100

    feat/data: add firebase onCreate function

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.onCreateFollower = functions.firestore
+  .document("/followers/{userId}/userFollowers/{followerId}")
+  .onCreate(async (snapshot, context) => {
+    console.log("Follower Created", snapshot.id);
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    // 1) Create a userPostsRef for followed users
+    const followedUserPostsRef = admin
+      .firestore()
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts");
+
+    // 2) Create timelinePostsRef for following users
+    const timelinePostsRef = admin
+      .firestore()
+      .collection("timeline")
+      .doc(followerId)
+      .collection("timelinePosts");
+
+    // 3) Get users posts from followed users
+    const querySnapshot = await followedUserPostsRef.get();
+
+    // 4) Add user posts to timeline of following users
+    querySnapshot.forEach(doc => {
+      if (doc.exists) {
+        const postId = doc.id;
+        const postData = doc.data();
+        timelinePostsRef.doc(postId).set(postData);
+      }
+    });
+  });
