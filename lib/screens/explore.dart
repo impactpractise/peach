9c99ba648cb288daa715ec7d0de98754bcde6a31
Andre <andre.bappert@gmail.com>
2020-03-14 19:46:40 +0100

    feat/data: show timeline in explorepage

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,61 +1,59 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:peach/models/user.dart';
+import 'package:peach/screens/home.dart';
 import 'package:peach/widgets/header.dart';
 import 'package:peach/widgets/loading.dart';
+import 'package:peach/widgets/post.dart';
 
 final usersRef = Firestore.instance.collection('users');
 
 class Explore extends StatefulWidget {
+  final User currentUser;
+
+  Explore({this.currentUser});
+
   @override
   _ExploreState createState() => _ExploreState();
 }
 
 class _ExploreState extends State<Explore> {
+  List<Post> posts;
   @override
   void initState() {
-    //createUser();
-    //updateUser();
-    deleteUser();
     super.initState();
+    getExplorePage();
   }
 
-  createUser() async {
-    await usersRef
-        .document("uniqueSampleString")
-        .setData({"username": "Niklas", "postsCount": 8, "isAdmin": false});
+  getExplorePage() async {
+    QuerySnapshot snapshot = await timelineRef
+        .document(widget.currentUser.id)
+        .collection('timelinePosts')
+        .orderBy('timestamp', descending: true)
+        .getDocuments();
+    List<Post> posts =
+        snapshot.documents.map((doc) => Post.fromDocument(doc)).toList();
+    setState(() {
+      this.posts = posts;
+      print(currentUser);
+    });
   }
 
-  updateUser() async {
-    final doc = await usersRef.document("uniqueSampleString").get();
-    if (doc.exists) {
-      doc.reference.updateData({"username": "Niklas Donges"});
+  buildExplorePage() {
+    if (posts == null) {
+      return circularProgress(context);
+    } else if (posts.isEmpty) {
+      return Text('No posts');
+    } else {
+      return PageView(children: posts, scrollDirection: Axis.vertical);
     }
   }
 
-  deleteUser() async {
-    final DocumentSnapshot doc =
-        await usersRef.document("uniqueSampleString").get();
-    if (doc.exists) {
-      doc.reference.delete();
-    }
-  }
-
+  @override
   Widget build(context) {
     return Scaffold(
         appBar: header(context, isAppTitle: true),
-        body: StreamBuilder<QuerySnapshot>(
-            stream: usersRef.snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return circularProgress(context);
-              }
-              final List<Text> children = snapshot.data.documents
-                  .map((doc) => Text(doc['username']))
-                  .toList();
-              return Container(
-                  child: ListView(
-                children: children,
-              ));
-            }));
+        body: RefreshIndicator(
+            onRefresh: () => getExplorePage(), child: buildExplorePage()));
   }
 }
