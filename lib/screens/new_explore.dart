ede93519c55c597400e8adc0ed4001e82efbcb21
Andre <andre.bappert@gmail.com>
2020-03-21 10:17:54 +0100

    feat/ui: add actionsToolBar

--- lib/screens/new_explore.dart
+++ lib/screens/new_explore.dart
@@ -27,7 +27,21 @@ class _NewExploreState extends State<NewExplore> {
             margin: EdgeInsets.only(top: 10))
       ]));
 
-  Widget get actionsToolbar => Container(width: 100, color: Colors.red[300]);
+  Widget get actionsToolbar => Container(
+        width: 100,
+        color: Colors.red[300],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: List<Widget>.generate(
+              5,
+              (_) => Container(
+                    width: 60,
+                    height: 60,
+                    color: Colors.blue[300],
+                    margin: EdgeInsets.only(top: 20),
+                  )),
+        ),
+      );
 
   Widget get middleSection => Expanded(
         child: Row(
